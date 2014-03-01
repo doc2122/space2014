75c36ca91cd54526f8d6137ed8728a0acd638c7d
brendag <brendag@uoregon.edu>
2014-02-28 22:29:04 -0800

    Added EVE twirl button to GUI and added twirl functionality to EVE (eve_with_texture.ma needs to be open in Maya for button to work)

--- src/mayapy/views/assignment1/Assignment1Widget.py
+++ src/mayapy/views/assignment1/Assignment1Widget.py
@@ -29,14 +29,67 @@ class Assignment1Widget(PyGlassWidget):
         This callback creates a polygonal cylinder in the Maya scene.
 
         """
-        r = 50
-        a = 2.0*r
-        y = (0, 1, 0)
-        c = cmds.polyCylinder(
-            r=r, h=5, sx=40, sy=1, sz=1, ax=y, rcp=0, cuv=2, ch=1, n='exampleCylinder')[0]
-        cmds.select(c)
-        response = nimble.createRemoteResponse(globals())
-        response.put('name', c)
+        # r = 50
+        # a = 2.0*r
+        # y = (0, 1, 0)
+        # c = cmds.polyCylinder(
+        #     r=r, h=5, sx=40, sy=1, sz=1, ax=y, rcp=0, cuv=2, ch=1, n='exampleCylinder')[0]
+        # cmds.select(c)
+        # response = nimble.createRemoteResponse(globals())
+        # response.put('name', c)
+        cmds.select('EVE')
+        height = 25.0
+        numTwirls = 2.5
+        interval = height / numTwirls
+        currentHeight = 0.0
+        rotateAngle = 0.0
+        stepSize = .02
+        count = 0;
+        cmds.setAttr('EVE.rotateY', 0.0)
+        armliftAngleR = 0.0
+        armliftAngleL = 0.0
+
+        while(currentHeight <= height):
+            # cmds.setAttr('EVE.rotateY', rotateAngle)
+            if(currentHeight >= 1*(height/2)):
+                stepSize = .015
+                if(armliftAngleL >= -50.0 and armliftAngleR <= 50):
+                    armliftAngleL -= .1
+                    armliftAngleR += .1
+
+
+            cmds.setKeyframe('arm_left', attribute='rotateX', value=armliftAngleL, t=count)
+            cmds.setKeyframe('arm_right', attribute='rotateX', value=armliftAngleR, t=count)
+            cmds.setKeyframe('EVE', attribute='translateY', value = currentHeight, t=count )
+            cmds.setKeyframe('EVE', attribute='rotateY', value = rotateAngle, t=count )
+            rotateAngle = rotateAngle + 1
+            #print("Keyframe i: " + str(count) + " Rotate angle: " + str(rotateAngle))
+            #cmds.setAttr('EVE.translateY', currentHeight)
+            currentHeight += stepSize
+            count += 1
+        # for i in range (360/2):
+        #     rotateAngle = rotateAngle + .5
+        #     cmds.setKeyframe('EVE', attribute='rotateY', value = rotateAngle, t=count )
+        #     count += 1
+        #stepSize = .015
+        while(currentHeight >= 0):
+            # cmds.setAttr('EVE.rotateY', rotateAngle)
+            if(armliftAngleL <=0 and armliftAngleR >= 0):
+                    armliftAngleL += .01
+                    armliftAngleR -= .01
+            cmds.setKeyframe('arm_left', attribute='rotateX', value=armliftAngleL, t=count)
+            cmds.setKeyframe('arm_right', attribute='rotateX', value=armliftAngleR, t=count)
+            cmds.setKeyframe('EVE', attribute='translateY', value = currentHeight, t=count )
+            cmds.setKeyframe('EVE', attribute='rotateY', value = rotateAngle, t=count )
+            rotateAngle = rotateAngle + .5
+            #print("Keyframe i: " + str(count) + " Rotate angle: " + str(rotateAngle))
+            #cmds.setAttr('EVE.translateY', currentHeight)
+            currentHeight -= stepSize
+            count += 1
+
+
+        print('Total key frames created: ' + str(count))
+
 #___________________________________________________________________________________________________ _handleReturnHome
     def _handleReturnHome(self):
         self.mainWindow.setActiveWidget('home')
